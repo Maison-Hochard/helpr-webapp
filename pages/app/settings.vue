38dad7c398763575b7ba426757bb2b1d83774974
HugoRCD <hugo.richard@epitech.eu>
2023-02-22 13:10:21 +0100

    Init theme selector

--- pages/app/settings.vue
+++ pages/app/settings.vue
@@ -7,8 +7,8 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="bg-secondary shadow rounded-lg">
-    <div class="px-4 py-5 sm:p-6">
+  <div class="flex flex-col gap-4">
+    <div class="bg-secondary px-4 py-5 sm:p-6 rounded-lg">
       <h3 class="text-lg font-medium leading-6 text-primary">Settings</h3>
       <div class="mt-2 max-w-xl text-sm text-muted">
         <p>Here you can change the application settings.</p>
@@ -18,13 +18,14 @@ definePageMeta({
         <LanguageSelector :isText="true" />
       </div>
     </div>
-    <div>
+    <div class="bg-secondary px-4 py-5 sm:p-6 rounded-lg">
       <h3 class="text-lg font-medium leading-6 text-primary">Theme</h3>
       <div class="mt-2 max-w-xl text-sm text-muted">
         <p>Here you can change the application theme.</p>
       </div>
       <div class="mt-5 flex gap-4">
         <ThemeSwitcher />
+      </div>
     </div>
   </div>
 </template>
