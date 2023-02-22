0ffdd3ea0b839e61742cb6e64db99de247516ea1
HugoRCD <hugo.richard@epitech.eu>
2023-02-22 21:34:06 +0100

    Init communities features

--- pages/app/profile/[userId].vue
+++ pages/app/profile/[userId].vue
@@ -1,9 +1,4 @@
-<template></template>
-
-<!--
 <script setup lang="ts">
-import { EnvelopeIcon, PhoneIcon, BackspaceIcon } from "@heroicons/vue/20/solid";
-
 const userId = useRoute().params.userId;
 
 const { data: user, error } = await useFetch("/api/getUserById", {
@@ -50,45 +45,30 @@ useHead({
               <div class="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
                 <div class="mt-6 min-w-0 flex-1 sm:hidden 2xl:block">
                   <h1 class="truncate text-2xl font-bold text-primary">{{ user.firstname }} {{ user.lastname }}</h1>
-                </div>
-                <div class="justify-stretch mt-6 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
-                  <button
-                    type="button"
-                    class="inline-flex justify-center rounded-md bg-accent px-4 py-2 text-sm font-medium text-inverted shadow-sm hover:bg-accent-hover"
-                  >
-                    <EnvelopeIcon class="-ml-1 mr-2 h-5 w-5 text-inverted" aria-hidden="true" />
-                    <span>Email</span>
-                  </button>
-                  <button
-                    type="button"
-                    class="inline-flex justify-center rounded-md bg-accent px-4 py-2 text-sm font-medium text-inverted shadow-sm hover:bg-accent-hover"
-                  >
-                    <PhoneIcon class="-ml-1 mr-2 h-5 w-5 text-inverted" aria-hidden="true" />
-                    <span>Call</span>
-                  </button>
+                  <div class="sm:col-span-2 mt-5">
+                    <dt class="text-sm font-medium text-gray-500">Bio</dt>
+                    <dd class="mt-1 max-w-prose space-y-5 text-sm text-primary" v-html="user.bio || bio" />
+                  </div>
                 </div>
               </div>
             </div>
             <div class="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
               <h1 class="truncate text-2xl font-bold text-primary">{{ user.firstname }} {{ user.lastname }}</h1>
+              <div class="sm:col-span-2 mt-5">
+                <dt class="text-sm font-medium text-gray-500">Bio</dt>
+                <dd class="mt-1 max-w-prose space-y-5 text-sm text-primary" v-html="user.bio || bio" />
+              </div>
             </div>
           </div>
         </div>
         <hr class="border-muted my-8" />
-        <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-          <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-            <div class="sm:col-span-2">
-              <dt class="text-sm font-medium text-gray-500">Bio</dt>
-              <dd class="mt-1 max-w-prose space-y-5 text-sm text-primary" v-html="user.bio || bio" />
-            </div>
-            <div>
-              <dt class="text-sm font-medium text-gray-500">Email</dt>
-              <dd class="mt-1 text-sm text-primary">{{ user.email }}</dd>
-            </div>
-          </dl>
+        <div id="public-flows" class="mt-8 m-6">
+          <div>
+            <h2 class="text-lg leading-6 font-medium text-primary">Public flows</h2>
+            <p class="mt-1 text-sm text-gray-500">This is a list of all public flows.</p>
+          </div>
         </div>
       </article>
     </main>
   </div>
 </template>
--->
