22e1b4a559a34dffeee3c691a6152ed536de73c5
paulmarniquet <paul.marniquet@epitech.eu>
2023-02-22 15:54:11 +0100

    Fix Google Auth

--- components/loginWithGoogle.vue
+++ components/loginWithGoogle.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-import { googleSdkLoaded } from "vue3-google-login";
+import * as gapi from "gapi-script";
+import {googleSdkLoaded} from "vue3-google-login";
+
 const login = () => {
+  const SCOPES = "email profile openid https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/gmail.compose https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.events.owned https://www.googleapis.com/auth/spreadsheets https://mail.google.com/ https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.metadata https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/pubsub";
   googleSdkLoaded((google) => {
     google.accounts.oauth2
-      .initTokenClient({
-        client_id: useRuntimeConfig().public.googleClientId,
-        scope:
-          "email profile openid https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/gmail.compose https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/spreadsheets.readonly https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.events.owned https://www.googleapis.com/auth/spreadsheets",
-        callback: async (response) => {
-          await addCredentials("google", response.access_token);
-        },
-      })
-      .requestAccessToken();
+          .initTokenClient({
+            client_id: useRuntimeConfig().public.googleClientId,
+            scope: SCOPES,
+                  callback: async (response) => {
+              await addCredentials("google", response.access_token);
+              },
+          })
+        .requestAccessToken();
   });
 };
 </script>
