8078512143e15d0d8baccdc709c77d0636d58a35
HugoRCD <hugo.richard@epitech.eu>
2023-02-26 19:25:23 +0100

    Improve navbar

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Disclosure, DisclosureButton, DisclosurePanel, Dialog, DialogPanel } from "@headlessui/vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
 import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 import LanguageSelector from "~/components/settings/LanguageSelector.vue";
 
@@ -14,11 +14,12 @@ const mobileMenuOpen = ref(false);
 
 <template>
   <div>
-    <header class="backdrop-blur-lg bg-primary-opacity/40 sticky top-0 z-10">
+    <header class="backdrop-blur-lg bg-primary-opacity/40 sticky top-0 z-50">
       <nav class="mx-auto flex max-w-7xl items-center justify-between py-3 px-4" aria-label="Global">
         <div class="flex flex-1">
           <div class="hidden lg:flex lg:gap-x-12">
             <NuxtLink
+              @click="mobileMenuOpen = false"
               v-for="item in navigation"
               :key="item.name"
               :to="item.to"
@@ -41,10 +42,16 @@ const mobileMenuOpen = ref(false);
         <div class="flex flex-1 justify-end items-center gap-x-4">
           <LanguageSelector />
           <div v-if="!user" class="hidden lg:flex lg:gap-x-4">
-            <NuxtLink to="/login" class="btn-primary py-1">{{ $t("navigation.login") }}</NuxtLink>
-            <NuxtLink to="/signup" class="btn-secondary py-1">{{ $t("navigation.signup") }}</NuxtLink>
+            <NuxtLink to="/login" class="btn-primary py-1" @click="mobileMenuOpen = false">{{
+              $t("navigation.login")
+            }}</NuxtLink>
+            <NuxtLink to="/signup" class="btn-secondary py-1" @click="mobileMenuOpen = false">{{
+              $t("navigation.signup")
+            }}</NuxtLink>
           </div>
-          <NuxtLink to="/app/my-flows" class="btn-secondary py-1" v-else>{{ $t("navigation.open_app") }}</NuxtLink>
+          <NuxtLink to="/app/my-flows" class="btn-secondary py-1 hidden md:block" v-else>{{
+            $t("navigation.open_app")
+          }}</NuxtLink>
         </div>
       </nav>
       <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
@@ -61,6 +68,7 @@ const mobileMenuOpen = ref(false);
           </div>
           <div class="mt-6 space-y-2">
             <NuxtLink
+              @click="mobileMenuOpen = false"
               v-for="item in navigation"
               :key="item.name"
               :to="item.to"
@@ -71,8 +79,12 @@ const mobileMenuOpen = ref(false);
           <div class="mt-10 flex flex-col gap-y-2">
             <LanguageSelector :is-text="true" />
             <div class="flex flex-col gap-y-2" v-if="!user">
-              <NuxtLink to="/login" class="btn-primary py-1">{{ $t("navigation.login") }}</NuxtLink>
-              <NuxtLink to="/signup" class="btn-secondary py-1">{{ $t("navigation.signup") }}</NuxtLink>
+              <NuxtLink to="/login" class="btn-primary py-1" @click="mobileMenuOpen = false">{{
+                $t("navigation.login")
+              }}</NuxtLink>
+              <NuxtLink to="/signup" class="btn-secondary py-1" @click="mobileMenuOpen = false">{{
+                $t("navigation.signup")
+              }}</NuxtLink>
             </div>
             <NuxtLink to="/app/my-flows" class="btn-secondary py-1" v-else>{{ $t("navigation.open_app") }}</NuxtLink>
           </div>
